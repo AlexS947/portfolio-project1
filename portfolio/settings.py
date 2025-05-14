6f09880ea69d2f7e2a2bfd4a0ff04220fb01f1f9
Alex <settasalex1@gmail.com>
2025-05-14 10:37:35 +0200

    Local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,6 +131,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
