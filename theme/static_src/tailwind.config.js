50ec5f3a7de96b565221c26e490f375e3a75783b
Alex <settasalex1@gmail.com>
2025-06-11 13:28:00 +0200

    Changed config.js file

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,12 +1,10 @@
-// theme/static_src/tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../blog/templates//*.html',
-    '../../../templates//*.html',
-    '../..//*.py',
-    '../static_src/src//*.js',
-    '../static_src/src//*.css',
+    '../../blog/templates/**/*.html',
+    '../../../templates/**/*.html',
+    '../../**/*.py',
+    '../static_src/src/**/*.js',
+    '../static_src/src/**/*.css',
   ],
   theme: {
     extend: {},
